be77b1751b0b7fc3b5282955963ef4f4afbee1fb
patdab90 <patdab90@gmail.com>
2015-05-24 02:50:56 +0200

    hibernate conf.

--- pacientnotifier/pncore/src/main/java/mu/study/siwoz/pncore/conf/Configuration.java
+++ pacientnotifier/pncore/src/main/java/mu/study/siwoz/pncore/conf/Configuration.java
@@ -1,10 +1,117 @@
 package mu.study.siwoz.pncore.conf;
 
+import java.beans.PropertyVetoException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import com.zaxxer.hikari.HikariDataSource;
 
 @org.springframework.context.annotation.Configuration
+@PropertySources(value = { @PropertySource(value = "classpath:/conf.properties") })
 public class Configuration {
 	@Autowired
 	private Environment environment;
+
+	/**
+	 * Configure properties to use @Value and ${...} placeholders.
+	 *
+	 * @return placeholder
+	 */
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		PropertySourcesPlaceholderConfigurer placeholder = new PropertySourcesPlaceholderConfigurer();
+		return placeholder;
+	}
+
+	/**
+	 * Hibernate jpa vendor adapter.
+	 *
+	 * @return jpa vendor adapter
+	 */
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		return new HibernateJpaVendorAdapter();
+	}
+
+	/**
+	 * Configure datasource.
+	 *
+	 * @return datasource
+	 * @throws PropertyVetoException
+	 */
+	@Bean(destroyMethod = "close")
+	public DataSource dataSource() throws PropertyVetoException {
+		HikariDataSource dataSource = new HikariDataSource();
+		dataSource.setDriverClassName(environment
+				.getProperty("jdbc.driverClass"));
+		dataSource.setJdbcUrl(environment.getProperty("jdbc.url"));
+		dataSource.setUsername(environment.getProperty("jdbc.user"));
+		dataSource.setPassword(environment.getProperty("jdbc.password"));
+		dataSource.setMinimumIdle(environment.getProperty(
+				"datasource.pool.min_size", Integer.class));
+		dataSource.setMaximumPoolSize(environment.getProperty(
+				"datasource.pool.max_size", Integer.class));
+		return dataSource;
+	}
+
+	/**
+	 * Spring entity manager factory.
+	 *
+	 * @param dataSource
+	 * @param jpaVendorAdapter
+	 * @return entity manager factory
+	 */
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+			DataSource dataSource, JpaVendorAdapter jpaVendorAdapter) {
+		LocalContainerEntityManagerFactoryBean springEMF = new LocalContainerEntityManagerFactoryBean();
+		springEMF.setDataSource(dataSource);
+		springEMF.setPackagesToScan("my.study.siwoz.pnweb.domain");
+		springEMF.setJpaVendorAdapter(jpaVendorAdapter);
+		Map<String, String> jpaProperties = new HashMap<String, String>();
+		jpaProperties.put("hibernate.dialect",
+				environment.getProperty("hibernate.dialect"));
+		jpaProperties.put("hibernate.show_sql",
+				environment.getProperty("hibernate.show_sql"));
+		jpaProperties.put("hibernate.format_sql",
+				environment.getProperty("hibernate.format_sql"));
+		jpaProperties.put("hibernate.hbm2ddl.auto",
+				environment.getProperty("hibernate.hbm2ddl.auto"));
+		jpaProperties
+				.put("hibernate.temp.use_jdbc_metadata_defaults",
+						environment
+								.getProperty("hibernate.temp.use_jdbc_metadata_defaults"));
+		springEMF.setJpaPropertyMap(jpaProperties);
+		return springEMF;
+	}
+
+	/**
+	 * Transaction manager.
+	 *
+	 * @param dataSource
+	 * @param entityManagerFactory
+	 * @return transaction manager
+	 */
+	@Bean(name = "txManager")
+	public JpaTransactionManager jpaTransactionManager(DataSource dataSource,
+			EntityManagerFactory entityManagerFactory) {
+		JpaTransactionManager tm = new JpaTransactionManager();
+		tm.setDataSource(dataSource);
+		tm.setEntityManagerFactory(entityManagerFactory);
+		return tm;
+	}
 }
